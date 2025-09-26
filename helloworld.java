89aa51c96c5ae01bb38edde92c34b7f3197cbfa8
ritiahuja-12 <ritiahuja2006@gmail.com>
2025-09-26 11:22:09 +0530

    Update helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+// Your first Java program
+ class helloworld {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
